3a94288bb13dc2b7a6bb12a6bcb6a10611a365b6
Etienne Kornobis <ekornobis@gmail.com>
2016-11-08 18:10:02 +0100

    Adding ploting length histogram for fas files

--- basics_fasta.py
+++ basics_fasta.py
@@ -1,6 +1,9 @@
+import sys
 import pysam
 import logging
 import basics_nuc_seq as bns
+import matplotlib.pyplot as plt
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -18,3 +21,11 @@ def fasta_stats(fasta):
             GC_content.append(bns.get_seq_GC(seq.sequence))
             N_count += seq.sequence.count("N")
 
+    plt.hist(seq_len, bins=range(0,300,10))
+    plt.show()
+
+
+if __name__ == "__main__":
+    fas = sys.argv[1]
+
+    fasta_stats(fas)
