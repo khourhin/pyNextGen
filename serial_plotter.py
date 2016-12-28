fe885f5ec3ddccdd418c665d823fbbd6db398bde
Etienne Kornobis <ekornobis@gmail.com>
2016-12-28 15:05:23 +0100

    Adding histogram bar plot

--- serial_plotter.py
+++ serial_plotter.py
@@ -1,21 +1,41 @@
-from bokeh.charts import Histogram, output_file, show
+from bokeh.charts import Bar, Histogram, output_file, show
 import sys
 import pandas as pd
 import argparse
+import q
+import types
+from collections import Counter
 
+# WARNINGS !!!
+# NOW working with only one column (for hist)
+# Assumes NO HEADER !
 
-def make_hist(data):
-    hist1 = Histogram(data)
+def hist(data):
+
+    hist_plot = Histogram(data)
 
     output_file('/tmp/serial_plotter_plot.html', title='Serial Plotter plot')
-    show(hist1)
+    show(hist_plot)
 
+def bar_hist(data):
+    
+    d = data['a'].value_counts()
+    print(d)
+    hist_plot = Bar(d, legend=None)
+    output_file('/tmp/serial_plotter_plot.html', title='Serial Plotter plot')
+    show(hist_plot)
 
+    
 def get_input(sep):
     """
     Return a panda dataframe from the data passed by the stdin
     """
-    df = pd.read_csv(sys.stdin, sep=sep)
+    
+    df = pd.read_csv(sys.stdin, sep=sep, header=None)
+
+    # Rename columns (necessary visibly to have column names for bokeh)
+    df.columns = list('abcdefghijklmnopqrstuvwxyz'[:len(df.columns)])
+    
     print(df)
     return df
 
@@ -26,10 +46,14 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--sep',
                         help='The character used for field separator in the stdin table',
                         default='\t')
-    parser.add_argument('-p', '--plot',
-                        help='The type of plot to use',
-                        default='hist')
+    parser.add_argument('--hist',
+                        help='Plot a classic histogram',
+                        dest='action', action='store_const', const=hist)
+    parser.add_argument('--barhist',
+                        help='Plot a bar plot histogram',
+                        dest='action', action='store_const', const=bar_hist)
+
     args = parser.parse_args()
 
-    make_hist(get_input(args.sep))
+    args.action(get_input(args.sep))
 
