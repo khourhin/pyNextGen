dd246e282394b09a8189ce061c9c1fde9b6aee85
Etienne Kornobis <ekornobis@gmail.com>
2017-03-07 14:00:08 +0100

    minor improvement of junctionSeq

--- R/junctionSeq.R
+++ R/junctionSeq.R
@@ -3,18 +3,21 @@
 ################################################################################
 
 ## Decoder is just a table with columns: samplenames, groups
-sampleData = '/home/ekornobis/analysis/batsche/badeg/1.1/junctionSeq/sample_groups.tab'
+#sampleData = '/home/ekornobis/analysis/batsche/badeg/1.1/junctionSeq/sample_groups.tab'
+sampleData = '/home/ekornobis/analysis/rachez/hp1g/code/input.tab'
 ## Directory where to find the qort output folder. HAS TO FINISH WITH "/" !!!!!!!!
-qortDir = '/home/ekornobis/analysis/batsche/badeg/1.1/qorts/'
-
-
+#qortDir = '/home/ekornobis/analysis/batsche/badeg/1.1/qorts/'
+qortDir = '/home/ekornobis/analysis/rachez/hp1g/star_out/qorts/'
+## Flattened GTF from qorts
+#gff.file = '/home/ekornobis/analysis/batsche/badeg/1.1/qorts/Homo_sapiens.GRCh37.75_FLAT.gtf'
+gff.file = '/home/ekornobis/analysis/rachez/hp1g/star_out/qorts/Mus_musculus.NCBIM37.67_FLAT.gtf'
 
 decoder = read.table(sampleData, col.names=c('ID', 'GROUP'), stringsAsFactors=FALSE)
 
+## Raw from sample group table
 countFiles = paste0(qortDir, decoder$ID, '/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt.gz')
-
-## From qorts
-gff.file = '/home/ekornobis/analysis/batsche/badeg/1.1/qorts/Homo_sapiens.GRCh37.75_FLAT.gtf'
+## OR with name modification
+countFiles = paste0(qortDir, gsub('.bam', '',decoder$ID), '/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt.gz')
 
 library(JunctionSeq)
 jscs <- runJunctionSeqAnalyses(sample.files = countFiles,
