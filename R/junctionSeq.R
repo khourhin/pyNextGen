6a6fc8ca2db1d6e64d290fe0941256d09cccf545
Etienne Kornobis <ekornobis@gmail.com>
2017-03-10 16:30:39 +0100

    Comments and adding parameters to directly add associated gene names

--- R/junctionSeq.R
+++ R/junctionSeq.R
@@ -1,18 +1,20 @@
-################################################################################
-                                        # STUB
-################################################################################
+# USAGE
+#Rscript ~/Programming/pyNextGen/R/junctionSeq.R TABLE-SAMPLE-CONDITION.tab QORT-FOLDER/ FLAT-GFF CORR-FILE
 
 options(bitmapType="cairo")
+library(JunctionSeq)
 
-## Decoder is just a table with columns: samplenames, groups
-#sampleData = '/home/ekornobis/analysis/batsche/badeg/1.1/junctionSeq/sample_groups.tab'
-sampleData = '/home/ekornobis/analysis/rachez/hp1g/code/input.tab'
+args = commandArgs(trailingOnly = TRUE)
+
+# Table with columns: samplenames, groups
+sampleData = args[1]
 ## Directory where to find the qort output folder. HAS TO FINISH WITH "/" !!!!!!!!
-#qortDir = '/home/ekornobis/analysis/batsche/badeg/1.1/qorts/'
-qortDir = '/home/ekornobis/analysis/rachez/hp1g/star_out/qorts/'
+qortDir = args[2]
 ## Flattened GTF from qorts
-#gff.file = '/home/ekornobis/analysis/batsche/badeg/1.1/qorts/Homo_sapiens.GRCh37.75_FLAT.gtf'
-gff.file = '/home/ekornobis/analysis/rachez/hp1g/star_out/qorts/Mus_musculus.NCBIM37.67_FLAT.gtf'
+gff.file = args[3]
+## Correspondance file (ENS names / associated gene names)
+corr.file = args[4]
+
 
 decoder = read.table(sampleData, col.names=c('ID', 'GROUP'), stringsAsFactors=FALSE)
 
@@ -21,13 +23,14 @@ countFiles = paste0(qortDir, decoder$ID, '/QC.spliceJunctionAndExonCounts.forJun
 ## OR with name modification
 countFiles = paste0(qortDir, gsub('.bam', '',decoder$ID), '/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt.gz')
 
-library(JunctionSeq)
+
 jscs <- runJunctionSeqAnalyses(sample.files = countFiles,
                                sample.names = decoder$ID,
                                condition=factor(decoder$GROUP),
                                flat.gff.file = gff.file,
                                nCores = 20,
-                               analysis.type = "junctionsAndExons"
+                               analysis.type = "junctionsAndExons",
+                               gene.names = corr.file
                                )
 
 writeCompleteResults(jscs,outfile.prefix="./test",save.jscs = TRUE)
@@ -36,4 +39,5 @@ buildAllPlots(jscs=jscs,
               outfile.prefix = "./plots/",
               use.plotting.device = "png",
               FDR.threshold = 0.01)
+
 
