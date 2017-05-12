6f70818d4ea30b0f136834dcc20b114beddcc389
Etienne Kornobis <ekornobis@gmail.com>
2017-05-12 16:54:53 +0200

    Adding sra wrapper and isoform plotting function

---
+++ R/isoform_compo_all_cond_lss.R
@@ -0,0 +1,39 @@
+library(grid)
+library(ggplot2)
+library(gridExtra)
+library(reshape2)
+
+df = read.table('/home/ekornobis/analysis/allemand/gphn/2017_01_18/total_clusters_nom/table_isoName_exonCode_freq_sorted.tab',header=TRUE)
+
+s = df[,1:41]
+s = melt(s, id='Name')
+
+# To keep the order in the input table
+s$Name = as.character(s$Name)
+s$Name = factor(s$Name, levels=rev(unique(s$Name)))
+
+colors=c('white','chartreuse4','red','red','red','red','red')
+#chartreuse4
+
+p1 = ggplot(s, aes(variable, Name, fill=factor(value))) +
+    geom_tile(colour="black", size=0.25) +
+    scale_fill_manual(values=colors) +
+    guides(fill=FALSE) +
+    xlab("Exons") + ylab("Isoforms") +
+    scale_y_discrete(breaks = c("GPHN_1","GPHN_50", "GPHN_100", "GPHN_150", "GPHN_200", "GPHN_250", "GPHN_277")) +
+    theme(axis.text.y = element_text(size=10), axis.title=element_text(size=20,face="bold"))
+
+# NOT FULLY WORKING (DOES NOT SORT FREQUENCIES)
+
+p2 = ggplot(df, aes(Name, freq)) +
+    geom_bar(stat="identity") +
+    xlab("") + ylab("Frequencies") + 
+    coord_flip() +
+    scale_y_log10() +
+    scale_y_continuous(trans='log10', breaks = c(2,5,10,25,50,100,200,500,2000,5000,20000)) +
+    theme(axis.ticks = element_blank(), axis.text.y=element_blank(), axis.title=element_text(size=20,face="bold"))
+
+#p2 = qplot(iso_freq, geom="bar", xlab = "",) + coord_flip() + theme(axis.ticks = element_blank())
+
+grid.arrange(p1,p2, widths=c(3/4, 1/4))
+dev.copy2pdf()
---
+++ basics_sra.py
@@ -0,0 +1,70 @@
+import argparse
+import subprocess
+
+# USAGE EX:
+#cd /home/ekornobis/analysis/batsche/methyleu/2.0_pileuping
+#SRAS=$(ls pups/ | cut -f1,1 -d'_' | sort -u)
+#grep CD44 ~/data/genomes/ensembl/h_sapiens/hg37/Homo_sapiens.GRCh37.75.gtf | head -n1 > j1
+#for sra in $SRAS; do python ~/code/batsche/methyleu/get_gene_specific_sra.py $sra j1 -f 10000; done
+
+class Gene():
+    """
+    Gene object parsed from a gtf entry
+    """
+
+    def __init__(self, gtf_entry):
+        gtf_entry = gtf_entry.split('\t')
+        self.chrom = gtf_entry[0]
+        self.start = int(gtf_entry[3])
+        self.end = int(gtf_entry[4])
+
+        # Parsing gtf 
+        attrs = [x.strip().split(' ') for x in gtf_entry[-1].split(';')[:-1]]
+        attrs = {x[0]:x[1].strip('"') for x in attrs}
+        
+        self.ensemblID = attrs['gene_id']
+        self.gene_name = attrs['gene_name']
+        
+    def __str__(self):
+
+        string = "ID: {ensemblID}\tNAME: {gene_name}\t{chr}:{start}-{end}".format(ensemblID=self.ensemblID,
+                                                                                 gene_name=self.gene_name,
+                                                                                 chr=self.chrom,
+                                                                                 start=self.start,
+                                                                                 end=self.end)
+        return string
+
+def get_bam_for_gene(sra, gene, flank):
+    """
+    """
+    print(gene)
+    bam = "{sra}_{gene_name}.bam".format(sra=sra, gene_name=gene.gene_name)
+    cmd = 'sam-dump --aligned-region {chr}:{start}-{end} {sra} | samtools view -bh > {bam}'.format(chr=gene.chrom,
+                                                                                                   start=gene.start - flank,
+                                                                                                   end=gene.end + flank,
+                                                                                                   bam=bam,
+                                                                                                   sra=sra)
+    print(cmd)
+    subprocess.check_output(cmd, shell=True)
+    
+def get_all_bams(sra, gene_tab, flank):
+    """
+    """
+    
+    with open(gene_tab, 'r') as f:
+        for line in f:
+            g = Gene(line)
+            get_bam_for_gene(sra, g, flank)
+        
+def main():    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sra', help='The SRR identifier to get the data from.')
+    parser.add_argument('gene_tab', help='A table file with $1: ENSEMBL ID, $2: gene name, $3: chromosome, $4: start, $5: end')
+    parser.add_argument('--flank', '-f', help='Number of bases to add on each flank of the gene interval', default=0, type=int)
+
+    args = parser.parse_args()
+
+    get_all_bams(args.sra, args.gene_tab, args.flank)
+
+if __name__ == "__main__":
+    main()
