3dedac46831f3bbf3c7d4df0661092d761553afd
Etienne Kornobis <ekornobis@gmail.com>
2017-06-27 10:53:20 +0200

    Removing debugging print

--- basics_fastq.py
+++ basics_fastq.py
@@ -8,33 +8,34 @@ from multiprocessing import Pool
 import argparse
 import os
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 handler = logging.FileHandler('basics_fastq.log')
 handler.setLevel(logging.INFO)
 
 # create a logging format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 
 # add the handlers to the logger
-log.addHandler(handler)
+logger.addHandler(handler)
 
 
 def iterate_fastq(fastq):
     """ Iterate through a fastq file """
-    
+
     with pysam.FastxFile(fastq) as fq:
         for read in fq:
             yield read
 
-            
+
 def filter_fastq(fastq, filter_fun):
     """ Filter a fastq file according to the specified filter """
 
     filtered_reads = set()
-    
+
     for read in iterate_fastq(fastq):
         if filter_fun(read):
             filtered_reads.add(read.name)
@@ -43,7 +44,7 @@ def filter_fastq(fastq, filter_fun):
     log.info('Done filtering {}'.format(fastq))
     return filtered_reads
 
-            
+
 def has_polyA(read, n=6):
     """
     SPECIFIC !!! For now only check if the read ends with n "A"
@@ -52,7 +53,7 @@ def has_polyA(read, n=6):
     if read.sequence.endswith("A" * n):
         return True
 
-    
+
 def has_polyT(read, n=6):
     """
     SPECIFIC !!! For now only check if the read starts with n "T"
@@ -61,23 +62,24 @@ def has_polyT(read, n=6):
     if read.sequence.startswith("T" * n):
         return True
 
-        
+
 def print_filter_fastq(fastq, read_set):
     """
     From a fastq file and a list of read name, produce a filtered
     fastq file with only the selected reads in.
     Output fastq will be with the suffixe '_filtered.fq'
     """
-    
+
     fastq_out = fastq.split(os.extsep)[0] + "_filtered.fq"
-    
+
     with open(fastq_out, 'w') as fout:
         for read in iterate_fastq(fastq):
             if read.name in read_set:
                 fout.write(str(read) + '\n')
-                
+
     log.info("Written filtered fastq to {}".format(fastq_out))
-            
+
+
 def fastq_stats(fastq):
     """
     Get stats from a fastq (or gz compressed fastq) file
@@ -121,7 +123,7 @@ def print_all_stats(fastqs_list, threads=10):
 
     p = Pool(threads)
     res_list = p.map(fastq_stats, [fq for fq in fastqs_list])
-        
+
     # header
     print(",".join(res_list[0].keys()))
 
@@ -129,38 +131,51 @@ def print_all_stats(fastqs_list, threads=10):
     for res in res_list:
         print(",".join([str(x) for x in res.values()]))
 
+
 def filter_polyA_reverse_stranded_reads(fq1, fq2):
     """
     Produce fastqs file with only reads containing (in either of the
     pair) a polyA tail. As it is IT WILL ONLY WORK WITH REVERSE
     STRANDED PAIRED DATA.
     """
-    
-    log.info("Starting polyA selection with pair: {0}; {1}".format(fq1,fq2))
+
+    log.info("Starting polyA selection with pair: {0}; {1}".format(fq1, fq2))
     read_set1 = filter_fastq(fq1, has_polyT)
     log.info('Found {0} polyT hits in {1}'.format(len(read_set1), fq1))
     read_set2 = filter_fastq(fq2, has_polyA)
     log.info('Found {0} polyA hits in {1}'.format(len(read_set2), fq2))
     read_set = read_set1 | read_set2
-    
+
     print_filter_fastq(fq1, read_set)
     print_filter_fastq(fq2, read_set)
 
-    
+
 def apply_threads(func, arg_list, nthreads=10):
     p = Pool(nthreads)
-    p.starmap(func, arg_list)        
+    p.starmap(func, arg_list)
 
-    
+
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fastq1', '-f1', nargs='+', help='A fastq file. First of pair. In the path, Dots "." should only separate extensions !')
-    parser.add_argument('--fastq2', '-f2', nargs='+', help='A fastq file. Second of pair. In the path, Dots "." should only separate extensions !')
+    parser.add_argument('--stats', '-s', nargs='+',
+                        help='At least 1 fastq file to produce stats from')
+    parser.add_argument('--fastq1', '-f1', nargs='+',
+                        help='FOR POLYA_ONLY_CHECK_CODE. A fastq file. First of pair. In the path, Dots "." should only separate extensions !')
+    parser.add_argument('--fastq2', '-f2', nargs='+',
+                        help='FOR POLYA_ONLY_CHECK_CODE. A fastq file. Second of pair. In the path, Dots "." should only separate extensions !')
+    parser.add_argument('--threads', '-t', help='Number of threads to use.')
     args = parser.parse_args()
 
-    if len(args.fastq1) != len(args.fastq2):
-        raise IOError("The number of files specified with --fastq1 and --fastq2 should be equal.")
+    if args.stats:
+        print_all_stats(args.stats)
 
-    apply_threads(filter_polyA_reverse_stranded_reads, zip(args.fastq1, args.fastq2))
+    elif len(args.fastq1) != len(args.fastq2):
+        raise IOError(
+            "The number of files specified with --fastq1 and --fastq2 should be equal.")
 
+    else:
+        apply_threads(filter_polyA_reverse_stranded_reads,
+                      zip(args.fastq1, args.fastq2))
+
+    
