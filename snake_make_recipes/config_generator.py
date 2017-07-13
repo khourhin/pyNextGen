4875a23352f981c502bb974351459d624af3aa5e
Etienne Kornobis <ekornobis@gmail.com>
2017-07-13 17:30:31 +0200

    Joining snakemake config generator and post bam snake pipeline

--- snake_make_recipes/config_generator.py
+++ snake_make_recipes/config_generator.py
@@ -2,10 +2,18 @@ import logging
 import click
 import json
 
+# Usage
+# cd /home/ekornobis/Programming/pyNextGen/snake_make_recipes/test
+# python ../config_generator.py -s stranded -p single -b ../../demo_data/ALL/bams -f ../../demo_data/ALL/fqs -g ~/data/genomes/ensembl/h_sapiens/hg37/Homo_sapiens.GRCh37.75.gtf
+# snakemake --cores 20 -s ../post_bam.sm --configfile snakemake.json
+# Limitations
+# Check the CMD_OPTIONS dictionnary for adding other programs
+
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-handler = logging.FileHandler('config_generator.log')
+handler = logging.FileHandler('/home/ekornobis/logs/config_generator.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
@@ -36,7 +44,7 @@ class Job():
     """Object representing a job submission to be used in a snakemake
     pipeline run. Basically this object gather all parameters passed
     to the command line and generate a json file to be used as a
-    --configFile in snakemake
+    --configfile in snakemake
     """
     
     def __init__(self, **kwargs):
@@ -58,9 +66,9 @@ class Job():
 
 
 @click.command()
-@click.option('--bamdir','-b', default=None, type=click.Path(exists=True), required=True,
+@click.option('--bamdir','-b', default=None, type=click.Path(exists=True, resolve_path=True),
               help='Path to the bam directory.')
-@click.option('--fqdir', '-f', default=None, type=click.Path(exists=True), required=True,
+@click.option('--fqdir', '-f', default=None, type=click.Path(exists=True, resolve_path=True), required=True,
               help='Path to the fastqs directory.')
 @click.option('--strand', '-s', type=click.Choice(['unstranded', 'stranded', 'reverse']), required=True,
               help='Strandedness: 0:not stranded; 1:stranded; 2: reverse stranded.')
@@ -68,8 +76,11 @@ class Job():
               help='Paired end: either True or False.')
 @click.option('--json_out', '-o', default='snakemake.json', type=str,
               help='Path to the json output.')
-@click.option('--gtf', '-g', default=None, type=click.Path(exists=True), required=True,
+@click.option('--gtf', '-g', default=None, type=click.Path(exists=True, resolve_path=True), required=True,
               help='Path to the gtf annotation file.')
+@click.option('--star_index','-i', default=None, type=click.Path(exists=True, resolve_path=True),
+              help='Path to the star index directory.')
+
 def main(**kwargs):
     """A simple parser to get NGS library information
     """
