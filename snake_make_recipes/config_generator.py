7ecf68615a10237481277a98707bfc1ea8d30ef2
Etienne Kornobis <ekornobis@gmail.com>
2017-07-13 14:57:45 +0200

    Refactoring config generator and handling pair and strand info

--- snake_make_recipes/config_generator.py
+++ snake_make_recipes/config_generator.py
@@ -10,41 +10,72 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+CMD_OPTIONS = {
+    'qorts': {'single': '--singleEnded',
+              'paired': '',
+              'unstranded': '',
+              'stranded': '--stranded --stranded_fr_secondstrand',
+              'reverse': '--stranded',
+    },
+    'qualimap': {'single': '',
+                 'paired': '-pe',
+                 'unstranded': '-p non-strand-specific',
+                 'stranded': '-p strand-specific-forward',
+                 'reverse': '-p strand-specific-reverse',
+    },
+    'featureCount': {'single': '',
+                     'paired': '-p',
+                     'unstranded': '-s 0',
+                     'stranded': '-s 1',
+                     'reverse': '-s 2',
+    },
+}
+
 
 class Job():
+    """Object representing a job submission to be used in a snakemake
+    pipeline run. Basically this object gather all parameters passed
+    to the command line and generate a json file to be used as a
+    --configFile in snakemake
+    """
+    
     def __init__(self, **kwargs):
         self.params = kwargs
-        
+
+        # Building argument string  
+        for prog in CMD_OPTIONS:
+            self.params[prog] = ' '.join([CMD_OPTIONS[prog][self.params[p]] for p in ['pair', 'strand']])
+
+            
     def __repr__(self):
         return "Job object: {0}".format(self.params)
 
+    
     def generate_config(self, json_out):
 
         with open(json_out, 'w') as out:
             out.write(json.dumps(self.params, indent=4))
 
 
-def init_job(**kwargs):
+@click.command()
+@click.option('--bamdir','-b', default=None, type=click.Path(exists=True), required=True,
+              help='Path to the bam directory.')
+@click.option('--fqdir', '-f', default=None, type=click.Path(exists=True), required=True,
+              help='Path to the fastqs directory.')
+@click.option('--strand', '-s', type=click.Choice(['unstranded', 'stranded', 'reverse']), required=True,
+              help='Strandedness: 0:not stranded; 1:stranded; 2: reverse stranded.')
+@click.option('--pair', '-p', default=False, type=click.Choice(['single', 'paired']), required=True,
+              help='Paired end: either True or False.')
+@click.option('--json_out', '-o', default='snakemake.json', type=str,
+              help='Path to the json output.')
+@click.option('--gtf', '-g', default=None, type=click.Path(exists=True), required=True,
+              help='Path to the gtf annotation file.')
+def main(**kwargs):
     """A simple parser to get NGS library information
     """
     
     job = Job(**kwargs)
     logger.info("Initializing: {0}".format(job))
-    return job
-
-
-@click.command()
-@click.option('--bamdir', default=None, type=click.Path(exists=True), required=True, help='Path to the bam directory.')
-@click.option('--fqdir', default=None, type=click.Path(exists=True), required=True, help='Path to the fastqs directory.')
-@click.option('--strand', default=0, type=int, required=True, help='Strandedness: 0:not stranded; 1:stranded; 2: reverse stranded.')
-@click.option('--paired', default=False, type=bool, required=True, help='Paired end: either True or False.')
-@click.option('--json_out', default='snakemake.json', type=str, help='Path to the json output.')
-@click.option('--gtf', default=None, type=click.Path(exists=True), required=True, help='Path to the gtf annotation file.')
-def main(**kwargs):
-    """A simple parser to get NGS library information
-    """
-    
-    job = init_job(**kwargs)
     job.generate_config(kwargs['json_out'])
 
     
