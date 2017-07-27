c1910a6255b7623c051102927ae60301cb0f4098
Etienne Kornobis <ekornobis@gmail.com>
2017-07-27 11:14:01 +0200

    Adding master snakemake slurm sbatch script

---
+++ snake_make_recipes/master_snake.cmd
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name masterSnake
+#SBATCH --output job_masterSnake_%j.out
+#SBATCH --error job_masterSnake_%j.err
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --qos long
+
+module load snakemake
+module load fastqc/0.11.5
+
+# The snakemake file with pipeline rules
+SM=pre_bam.sm
+
+# Cluster specification for each job
+CLUS_CONF=cluster.conf
+
+# This configuration file can be create with config_generator.py
+SM_JSON=snakemake.json
+
+snakemake -j 10 --cluster-config $CLUS_CONF --cluster "sbatch -t {cluster.time}" -s $SM --configfile $SM_JSON
