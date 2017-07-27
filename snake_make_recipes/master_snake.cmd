48852ee50d607e4179987d8c182744cb06e64211
Etienne Kornobis <ekornobis@gmail.com>
2017-07-27 12:29:36 +0200

    updates master cmd and threads

--- snake_make_recipes/master_snake.cmd
+++ snake_make_recipes/master_snake.cmd
@@ -6,8 +6,11 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --qos long
 
-module load snakemake
+module load snakemake/3.5.4
 module load fastqc/0.11.5
+module load STAR/2.5.0a
+module load subread/1.4.6-p3
+module load MultiQC/0.7
 
 # The snakemake file with pipeline rules
 SM=pre_bam.sm
@@ -18,4 +21,4 @@ CLUS_CONF=cluster.conf
 # This configuration file can be create with config_generator.py
 SM_JSON=snakemake_tars_conf_example.json
 
-snakemake -j 10 --cluster-config $CLUS_CONF --cluster "sbatch -t {cluster.time}" -s $SM --configfile $SM_JSON
+snakemake -j 12 --cluster-config $CLUS_CONF --cluster "sbatch -t {cluster.time}" -s $SM --configfile $SM_JSON
