9d42a91bc71ff4e2617105bbcf41d84dfa2ef819
Etienne Kornobis <ekornobis@gmail.com>
2017-09-07 15:04:19 +0200

    Correcting typo

--- snake_make_recipes/master_snake.cmd
+++ snake_make_recipes/master_snake.cmd
@@ -16,7 +16,7 @@ module load MultiQC/0.7
 SM=~/Programming/pyNextGen/snake_make_recipes/pre_bam.sm 
 
 # Cluster specification for each job
-CLUS_CONF==~/Programming/pyNextGen/snake_make_recipes/cluster.conf
+CLUS_CONF=~/Programming/pyNextGen/snake_make_recipes/cluster.conf
 
 # This configuration file can be create with config_generator.py
 SM_JSON=snakemake.json
