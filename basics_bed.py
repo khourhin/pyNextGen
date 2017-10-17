184eada77e9cd4c2db45c3ce319d9e3713d3b36c
Etienne Kornobis <ekornobis@gmail.com>
2017-10-17 15:26:44 +0200

    Remove old bed library and replace with pybedtools usage

--- basics_bed.py
+++ basics_bed.py
@@ -1,86 +1,106 @@
-# remember that pybedtools exists !!
-import random
-import sys
+#! /usr/bin/env python3
+
+# Now using pybedtools
+from pybedtools import BedTool
+import argparse
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import click
+import logging
+import os
+from itertools import combinations
+from multiprocessing import Pool
+import pandas as pd
 from mylog import get_logger
 
 logger = get_logger(__file__, __name__)
 
 
-class BedInterval():
-    def __init__(self, bedline_list):
-        self.chrom = bedline_list[0]
-        self.start = int(bedline_list[1])
-        self.end = int(bedline_list[2])
-        self.length = self.end - self.start
+def get_genome_coverage(bedObj, genome_size):
+    """
+    Get an estimation of the proportion of the genome covered by all
+    the intervals present in the bed object.
+    """
 
-    def rand_cut(self, fragl):
-        """
-        Cut a random fragment fo length "fragl" from the interval
-        """
-        try:
-            c_start = random.randint(self.start, self.end - fragl)
-        except ValueError as e:
-            raise(e)
+    bedObj = bedObj.sort().merge()
+    cov = sum([len(i) for i in bedObj]) / genome_size
 
-        return BedInterval([self.chrom, c_start, c_start + fragl])
-
-    def __repr__(self):
-        return "{0}\t{1}\t{2}".format(self.chrom, self.start, self.end)
+    return cov
 
 
-class BedFile():
-    def __init__(self, bedfile):
-        self.intervals = [BedInterval(line.split())
-                          for line in open(bedfile, 'r')]
-        self.name = bedfile
+def bed_stats(bedObj, genome_size=None):
+    """
+    Produce various stats on a bed file
+    """
 
-    def rand_interval(self, min_size=None):
-        """
-        Return an randomly chose interval from a bed list.  If min_size is
-        specified, intervals will be sampled until they can form an
-        interval of minimal size min_size
-        """
+    lengths = [len(i) for i in bedObj]
 
-        sel_int = random.choice(self.intervals)
-        if min_size:
-            while sel_int.length < min_size:
-                sel_int = random.choice(self.intervals)
+    stats = {
+        'length': len(bedObj),
+        'smallest': min(lengths),
+        'largest': max(lengths),
+        'mean': np.mean(lengths),
+    }
 
-        return sel_int
+    if genome_size:
+        stats['genome_coverage'] =  get_genome_coverage(bedObj, genome_size)
 
-    def get_stats(self):
-        """
-        Get stats on the bed file
-        """
-        inters_len = [inter.length for inter in self.intervals]
-        stats = {
-            'nInt': len(self.intervals),
-            'minLen': min(inters_len),
-            'maxLen': max(inters_len),
-            'meanLen': np.mean(inters_len),
-            'medianLen': np.median(inters_len),
-        }
-        return stats
+    
+    [logger.info('{0}:{1}'.format(k, v)) for k, v in stats.items()]
 
-    def __repr__(self):
-        return "Bedfile Object for file {}".format(self.name)
+    return stats
+
+    
+def count_intersections(bed_tuple):
+    """Return a normalized (by the total number of feature of both beds)
+    intersetion counts
+    """
+
+    bed1 = bed_tuple[0]
+    bed2 = bed_tuple[1]
+    
+    logger.info('Comparing: {0} VS {1}'.format(bed1.fn, bed2.fn))
+
+    bed_inter = bed1.intersect(bed2)
+
+    # Normnalized number of intersections
+    # (divided by the total number of features in both files)
+    
+    return(bed1.fn,
+           bed2.fn,
+           len(bed_inter) / (len(bed1) + len(bed2)))
 
 
+def count_all_intersections(beds):
+
+    p = Pool(20)
+    inter_df = p.map(count_intersections, combinations(beds, 2))
+    inter_df = pd.DataFrame(inter_df, columns=['bed1', 'bed2', 'normCounts'])
+
+    return(inter_df)
+
+    
+@click.command()
+@click.argument('beds', required=True, nargs=-1)
+@click.option('--genome_size', type=int)
+def main(beds, genome_size):
+
+    beds = (BedTool(bed) for bed in beds)
+    inter_df = count_all_intersections(beds)
+    inter_df.to_csv('out2.csv')
+
+    
 if __name__ == "__main__":
+    main()
 
-    # From 2 beds:
-    # Get the lengths of the intervals in ref_bed
-    # Produce random intervals from sampled_bed with
-    # same lengths as ref_bed intervals
+    
+    # FOR LEGACY, TO RECONNECT PAST FUNCTION (NOT WORKING NOW)
+    
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('bed', help='A bed file')
+    # parser.add_argument('genome_size', type=int, help='The total number of bases of the considered genome')
+    # args = parser.parse_args()
 
-    ref_bed = sys.argv[1]
-    sampled_bed = sys.argv[2]
-
-    ref_bed = BedFile(ref_bed)
-    sampled_bed = BedFile(sampled_bed)
-
-    for inter in ref_bed.intervals:
-        sample = sampled_bed.rand_interval(inter.length)
-        print(sample.rand_cut(inter.length))
+    # bedObj = BedTool(args.bed)
+    # bed_stats(bedObj, args.genome_size)
