48a027e0778bde887b47e3d6f67ffe4b79323695
Etienne Kornobis <ekornobis@gmail.com>
2017-12-29 11:34:33 +0100

    Improving post DR analysis

--- post_DR_lib.py
+++ post_DR_lib.py
@@ -2,57 +2,63 @@ import pandas as pd
 import os
 import math
 
+# REQUIREMENTS
+# The DR tables should be created with the new version of DR notebook:
+# http://localhost:8888/notebooks/Programming/jupyter/good_practices/DEG_DESeq2_1.0.ipynb
 
-def filter_FDR_logFC(df, FDR_thres=0.05, logFC_thres=0):
-    """filter a DESeq2 result with FDR and logFC thresholds
+
+class Deseq2Results(object):
+    """Documentation for Deseq2Results
+
     """
+    def __init__(self, csv_file):
+        self.path = csv_file
+        self.name = os.path.splitext(os.path.basename(csv_file))[0]
+        self.df = self.read_de_res(csv_file)
+        self.add_fold_change()
 
-    logFC_filt =  df.filter(regex='.*\log2FoldChange').abs() >= logFC_thres
-    FDR_filt = df.filter(regex='.*\padj') < FDR_thres
+    def read_de_res(self, csv_file):
 
-    # Verify there is only one column for each
-    assert (logFC_filt.shape[1] == 1 & FDR_filt.shape[1] == 1)
-
-    return df[logFC_filt.values & FDR_filt.values]
-
-def describe_DEG(df):
-    """Summarize a DESEQ2 DEG table
-    """
-
-    total_number_DE = int(df.shape[0])
-    num_up_reg = int(((df.filter(regex='.*\log2FoldChange') > 0) == True).sum())
-    num_down_reg = int(((df.filter(regex='.*\log2FoldChange') < 0) == True).sum())
+        df = pd.read_csv(csv_file, index_col=0)
+        
+        if list(df.columns) != ['external_gene_name', 'chromosome_name', 'start_position',
+                                'end_position', 'strand', 'baseMean', 'log2FoldChange', 'lfcMLE',
+                                'lfcSE', 'stat', 'pvalue', 'padj']:
+            raise ValueError('The DR table does not have the correct headers.')
     
-    # Sanity check
-    assert(total_number_DE == num_up_reg + num_down_reg)
+        return df
 
-    return({'total_num_DE':total_number_DE, 'num_down_reg':num_down_reg,
-            'num_up_reg':num_up_reg})
+    def filter_fdr_fc(self, fdr_thres=0.05, fc_thres=0):
+        """filter a DESeq2 result with FDR and logFC thresholds
+        """
 
-def extract_gene_names_for_GO(df):
-    """ Return a file with one Gene IDs per line for further GO analysis
-    """
+        fc_filt = self.df['fold_change'].abs() >= fc_thres
+        fdr_filt = self.df['padj'] < fdr_thres
 
-    assert(df.index.name)
+        return self.df[fc_filt.values & fdr_filt.values]
 
-    outfile = os.path.splitext(df.index.name)[0] + "geneIDs_list_for_GO.txt"
-    with open(outfile, 'w') as f:
-        for gene_ID in df.index:
-            f.write(gene_ID + '\n')
+    def add_fold_change(self):
+        """ Transform logFC in FC and add a column top df
+        """
 
-def read_de_res(df_fname):
+        self.df['fold_change'] = [math.copysign(2**x, x) for x in self.df['log2FoldChange']]
 
-    df = pd.read_csv(df_fname, index_col=0)
-    # Trick to keep the name of the file used to generate the df
-    df.index.name = df_fname
-    df.col
 
-    return df
+    def extract_gene_names_for_GO(self, df):
+        """ Return a file with one Gene IDs per line for further GO analysis
+        """
+               
+        outfile = os.path.splitext(self.name) + "geneIDs_list_for_GO.txt"
+        with open(outfile, 'w') as f:
+            for gene_ID in df.index:
+                f.write(gene_ID + '\n')
 
-def add_fold_change(df):
+    def describe(self):
+        """ Describe the DR results
+        """
 
-    df = df['fold_change'] = [math.copysign(2**x, x) for x in df.filter(regex='.*\log2FoldChange')]
-    
+        self.filter_fdr_logfc(self)
+        
 
 def get_all_results(deseq2_de_files, FDR_thres=0.05, logFC_thres=0):
 
