232a1bf2b35770e9bcd40c177babbfb2c2326369
Etienne Kornobis <ekornobis@gmail.com>
2018-01-30 14:57:31 +0100

    Misspelling correction

--- snake_make_recipes/master_snake.cmd
+++ snake_make_recipes/master_snake.cmd
@@ -30,7 +30,7 @@ SM_JSON=snakemake.json
 # Logs directory (HAS TO FINISH WITH "/")
 #LOGS_DIR=~/logs/snakemake/$(date +%Y-%m-%d-%H-%M-%S)/;
 # Another Good possible locations for logs could be just the working directory
-LOGS_DIR=logs_cluster/X$(date +%Y-%m-%d-%H-%M-%S)/;
+LOGS_DIR=logs_cluster/$(date +%Y-%m-%d-%H-%M-%S)/;
 mkdir -p $LOGS_DIR
 
 snakemake -j 12 --printshellcmds --cluster-config $CLUS_CONF --cluster "sbatch --qos {cluster.qos} --ntasks {cluster.tasks} --cpus-per-task {cluster.cpus} --job-name {cluster.jobName} --output $LOGS_DIR{cluster.output} --error $LOGS_DIR{cluster.error}" -s $SM --configfile $SM_JSON
