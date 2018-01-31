671c9d92da658d44514a8cb702e56237345d4087
Etienne Kornobis <ekornobis@gmail.com>
2018-01-31 09:20:02 +0100

    Improved pca plot and summary

--- R/pca.R
+++ R/pca.R
@@ -5,9 +5,6 @@ library(repr)
 library(biomaRt)
 
 
-
-
-
 get.normalized.counts = function(counts, meta){
     ## Import data and normalization
 
@@ -43,10 +40,9 @@ get.loadings = function(counts, PC='PC1', n=10){
 }
 
 do.pca = function(counts, meta, file, gene_sel=NULL, lib_sel=NULL,
-                  repel_annot=TRUE, legend=TRUE,
-                  div=10000, annot_set=NULL, annot_cor=NULL, 
+                  repel_annot=TRUE, legend=TRUE, annot_set=NULL, annot_cor=NULL, 
                   annot_cor_col=2, logTrans=F, col_group_choice=1,
-                 shape_group_choice=1){
+                  shape_group_choice=1){
     
     # counts: the count matrix 
     # meta: a table with sample names as first columns and extra columns are groups 
@@ -55,7 +51,6 @@ do.pca = function(counts, meta, file, gene_sel=NULL, lib_sel=NULL,
     # lib_sel: A list of libraries to select
     # repel_annot: boolean, if using or not repel_annot
     # legend: boolean, printing legend or not
-    # div: The number by which the PC values are divided
     # annot_set: TO add further annotations to the plots
     # annot_cor: TO ADD
     # annot_cor_col: TO ADD
@@ -76,8 +71,9 @@ do.pca = function(counts, meta, file, gene_sel=NULL, lib_sel=NULL,
     }
     
     pca = prcomp(t(counts))
+    print(summary(pca))
     groups = meta[rownames(pca$x),, drop=FALSE] 
-    scores=data.frame(groups, pca$x[,1:3] / div)
+    scores=data.frame(groups, pca$x[,1:3])
     
     # THIS SHOULD BE IMPROVED !
     # I think the proper way will be to have groups defined as columns in 'scores' rather than outside
@@ -86,9 +82,11 @@ do.pca = function(counts, meta, file, gene_sel=NULL, lib_sel=NULL,
     geom_point(aes(color=factor(groups[,col_group_choice]),
                    shape=factor(groups[,shape_group_choice])),
                show.legend=legend) +
-    labs(x=paste('PC1 (/', div,')', sep=""), y=paste('PC2 (/', div,')', sep="")) + 
-    theme_classic() +
-    theme(axis.text = element_text(size = 12))
+        labs(x='PC1', y='PC2') + 
+        theme_classic() +
+        theme(axis.text = element_text(size = 12))
+
+    
     
     if (repel_annot){
         
