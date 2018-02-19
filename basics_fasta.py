45fe0c23741dfa243b31d20d0fd86442d7a96a64
Etienne Kornobis <ekornobis@gmail.com>
2018-02-19 17:03:38 +0100

    Adding fasta to dict conversion for fasta querying

--- basics_fasta.py
+++ basics_fasta.py
@@ -15,6 +15,9 @@ logger = get_logger(__file__, __name__)
 # TODO: check if dedup_fasta can be improved using yield as in
 # /home/ekornobis/code/allemand/gphn/7.2_generate_ref_before_rsem.py
 
+# TO IMPROVE:
+# Fasta method to_dict
+
 
 class Fasta(object):
     """A Fasta sequence file object
@@ -29,6 +32,41 @@ class Fasta(object):
     def __repr__(self):
         return '<Fasta Object for: {}'.format(self.path)
 
+    def to_dict(self, simple_ID=True):
+        """
+        Get a dictionnary from fasta file with:
+        key = seq_id
+        value = seq
+
+        If simple_ID is True, then the seq_id in the dictionnary will be
+        the original one truncated after the first white space.
+        """
+        with open(self.path, "r") as f:
+            seq_d = {}
+            
+            # split each time a ">" in encountered
+            fasta = f.read().split('>')[1:]
+            
+            # split the strings in 3 items tuple: seq id, the sep, and the seq
+            tmp = [seq.partition("\n") for seq in fasta]
+            
+            # build a dictionnary with key=seq id, value=seq
+            for i in range(len(tmp)):
+            
+                if simple_ID:
+                    # for ex: for trinity output, split()[0] removes the len, path infos
+                    seq_d[tmp[i][0].split()[0]] = tmp[i][2].replace("\n", "").upper()                
+                else:
+                    seq_d[tmp[i][0]] = tmp[i][2].replace("\n", "").upper()
+
+        # Check if no empty sequences
+        empty_seqs = [k for k in seq_d if len(seq_d[k]) == 0]
+        if empty_seqs:
+            raise IOError("Something seems wrong with this sequence:\n" 
+                          + "\n".join(empty_seqs))
+        
+        return seq_d
+    
     def get_stats(self):
         """
         Produce statistics on the sequences in the fasta file
@@ -52,8 +90,12 @@ class Fasta(object):
         """
         
         fasta_df = self.get_stats()
+        plt.subplot(1,2,1)
         plt.boxplot(fasta_df['seq_len'])
         plt.title('Sequence length boxplot')
+        plt.subplot(1,2,2)
+        plt.hist(fasta_df['seq_len'], bins=100, log=True)
+        plt.title('Sequence length Histogram(log)')
         plt.show()
 
         print(fasta_df.describe())
